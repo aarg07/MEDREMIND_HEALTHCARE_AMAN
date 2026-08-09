232a2db31e36857f64c5fed5d743cf6f2be74003
Auto Upload <auto@local>
2026-08-09 19:52:02 +0530

    Updated folder

---
+++ gh_jobs.py
@@ -0,0 +1,5 @@
+﻿import urllib.request, json
+url='https://api.github.com/repos/aarg07/MEDREMIND_HEALTHCARE_AMAN/actions/runs/31316947082/jobs'
+with urllib.request.urlopen(url) as r:
+    data=json.load(r)
+print(json.dumps({'total_count':data.get('total_count'), 'jobs':[{'id':j['id'],'name':j['name'],'status':j['status'],'conclusion':j['conclusion'],'html_url':j['html_url'],'logs_url':j['logs_url']} for j in data.get('jobs',[])]}, indent=2))
